0c2bed7ba8e6e9742b2f2e578e6cd22df8f23c49
Sam_Neve <samuelneve1@gmail.com>
2022-04-06 18:15:56 -0600

    Update RUNFIRST.sh

--- RUNFIRST.sh
+++ RUNFIRST.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-touch > /var/log/messages.3
-touch > /var/log/messages.4
-touch > /var/log/faillog.1
+touch /var/log/messages.3
+touch /var/log/messages.4
+touch /var/log/faillog.1
 
 chmod +x main.sh
